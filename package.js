c4d67223ce9dad71ce80e2be31a71893a4b29ec6
nriekert <83132156+nriekert@users.noreply.github.com>
2023-12-15 20:46:43 -0800

    Update package.js

--- package.js
+++ package.js
@@ -1,1 +1,22 @@
-
+{
+  "name": "Chat-Website",
+  "version": "1.0.0",
+  "description": "",
+  "main": "server.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/cyclic-software/starter-express-api.git"
+  },
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/cyclic-software/starter-express-api/issues"
+  },
+  "homepage": "https://github.com/cyclic-software/starter-express-api#readme",
+  "dependencies": {
+    "express": "^4.18.2"
+  }
+}
